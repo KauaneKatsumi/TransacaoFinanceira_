7f01d3a4a22642d1d469ac6e1d4c152b9f19c5e0
kauanekatsumi <kauanem2@gmail.com>
2025-09-29 22:29:34 -0300

    TransacaoFinanceira.cs fixed

--- TransacaoFinanceira/obj/Debug/net8.0/TransacaoFinanceira.AssemblyInfo.cs
+++ TransacaoFinanceira/obj/Debug/net8.0/TransacaoFinanceira.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransacaoFinanceira")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095a17e0cde660891c6b4c128a0d520c102334ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd477924e40ee4093f30959c5d9c7bfe03e15ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransacaoFinanceira")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransacaoFinanceira")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
